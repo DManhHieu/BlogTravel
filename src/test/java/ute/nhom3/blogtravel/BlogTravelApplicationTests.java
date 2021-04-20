972f595e9f155c240a81962828e1086800cb67f2
Đoàn Mạnh Hiếu <61335680+DManhHieu@users.noreply.github.com>
2021-04-20 10:23:09 +0700

    init

---
+++ src/test/java/ute/nhom3/blogtravel/BlogTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package ute.nhom3.blogtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
