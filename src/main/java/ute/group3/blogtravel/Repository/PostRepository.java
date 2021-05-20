508da125cfec25809d26c2ba3cf8c674606edb03
Đoàn Mạnh Hiếu <61335680+DManhHieu@users.noreply.github.com>
2021-05-20 21:38:08 +0700

    update css js

--- src/main/java/ute/group3/blogtravel/Repository/PostRepository.java
+++ src/main/java/ute/group3/blogtravel/Repository/PostRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import ute.group3.blogtravel.model.Post;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface PostRepository extends MongoRepository<Post,String> {
     Post findTopByOrderByNumberDesc();
     Post findByNumber(int number);
+    List<Post> findByUsername(String username);
 }
